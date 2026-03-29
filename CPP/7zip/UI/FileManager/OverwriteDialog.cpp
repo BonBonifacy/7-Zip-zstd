5e514880664bc9332ebc0bfa407ccc71a6c4b8f2
bonbonifacy <bonbonbonbonifacy@gmail.com>
2026-03-29 16:53:58 +0800

    feat: implement OverwriteDialog for file management operations

--- CPP/7zip/UI/FileManager/OverwriteDialog.cpp
+++ CPP/7zip/UI/FileManager/OverwriteDialog.cpp
@@ -64,8 +64,7 @@ void COverwriteDialog::SetItemIcon(unsigned iconID, HICON hIcon)
     DestroyIcon(hIcon);
 }
 
-void AddSizeValue(UString &s, UInt64 value);
-void AddSizeValue(UString &s, UInt64 value)
+static void AddSizeValue(UString &s, UInt64 value)
 {
   {
     wchar_t sz[32];
@@ -118,37 +117,7 @@ void COverwriteDialog::SetFileInfoControl(
     }
     SetItemText(textID, s);
   }
-/*
-  SHGetFileInfo():
-    DOCs: If uFlags does not contain SHGFI_EXETYPE or SHGFI_SYSICONINDEX,
-          the return value is nonzero if successful, or zero otherwise.
-    We don't use SHGFI_EXETYPE or SHGFI_SYSICONINDEX here.
-  win10: we call with SHGFI_ICON flag set.
-    it returns 0: if error : (shFileInfo::*) members are not set.
-    it returns non_0, if successful, and retrieve:
-      { shFileInfo.hIcon != NULL : the handle to icon (must be destroyed by our code)
-        shFileInfo.iIcon is index of the icon image within the system image list.
-      }
-  Note:
-    If we send path to ".exe" file,
-    SHGFI_USEFILEATTRIBUTES flag is ignored, and it tries to open file.
-    and return icon from that exe file.
-    So we still need to reduce path, if want to get raw icon of exe file.
-    
-  if (name.Len() >= MAX_PATH))
-  {
-    it can return:
-      return 0.
-      return 1 and:
-        { shFileInfo.hIcon != NULL : is some default icon for file
-          shFileInfo.iIcon == 0
-        }
-    return results (0 or 1) can depend from:
-      - unicode/non-unicode
-      - (SHGFI_USEFILEATTRIBUTES) flag
-      - exact file extension (.exe).
-  }
-*/
+
   int iconIndex = -1;
   for (unsigned i = 0; i < 2; i++)
   {
@@ -161,31 +130,20 @@ void COverwriteDialog::SetFileInfoControl(
           (!StringsAreEqualNoCase_Ascii(name.RightPtr(4), ".exe") &&
            !StringsAreEqualNoCase_Ascii(name.RightPtr(4), ".ico")))
         break;
-      // if path for ".exe" file is long, it returns default icon (shFileInfo.iIcon == 0).
-      // We don't want to show that default icon.
-      // But we will check for default icon later instead of MAX_PATH check here.
-      // if (name.Len() >= MAX_PATH) break; // optional
     }
     else
     {
-      // we need only file extension with dot
       const int separ = name.ReverseFind_PathSepar();
       name.DeleteFrontal((unsigned)(separ + 1));
-      // if (name.Len() >= MAX_PATH)
       {
         const int dot = name.ReverseFind_Dot();
         if (dot >= 0)
           name.DeleteFrontal((unsigned)dot);
-        // else name.Empty(); to set default name below
       }
-      // name.Empty(); // for debug
     }
 
     if (name.IsEmpty())
     {
-      // If we send empty name, SHGetFileInfo() returns some strange icon.
-      // So we use common dummy name without extension,
-      // and SHGetFileInfo() will return default icon (iIcon == 0)
       name = "__file__";
     }
 
@@ -198,25 +156,18 @@ void COverwriteDialog::SetFileInfoControl(
     }
 
     SHFILEINFO shFileInfo;
-    // ZeroMemory(&shFileInfo, sizeof(shFileInfo)); // optional
-    shFileInfo.hIcon = NULL; // optional
-    shFileInfo.iIcon = -1;   // optional
-    // memset(&shFileInfo, 1, sizeof(shFileInfo)); // for debug
+    shFileInfo.hIcon = NULL;
+    shFileInfo.iIcon = -1;
     const DWORD_PTR res = ::SHGetFileInfo(name, attrib,
         &shFileInfo, sizeof(shFileInfo),
         SHGFI_ICON | SHGFI_LARGEICON | SHGFI_SHELLICONSIZE |
-        // (i == 0 ? SHGFI_USEFILEATTRIBUTES : 0)
         SHGFI_USEFILEATTRIBUTES
-        // we use SHGFI_USEFILEATTRIBUTES for second icon, because
-        // it still returns real icon from exe files
         );
     if (res && shFileInfo.hIcon)
     {
-      // we don't show second icon, if icon index (iIcon) is same
-      // as first icon index of first shown icon (exe file without icon)
       if (   shFileInfo.iIcon >= 0
           && shFileInfo.iIcon != iconIndex
-          && (shFileInfo.iIcon != 0 || i == 0)) // we don't want default icon for second icon
+          && (shFileInfo.iIcon != 0 || i == 0))
       {
         iconIndex = shFileInfo.iIcon;
         SetItemIcon(i == 0 ? iconID : iconID_2, shFileInfo.hIcon);
@@ -226,8 +177,6 @@ void COverwriteDialog::SetFileInfoControl(
     }
   }
 }
-
-
 
 bool COverwriteDialog::OnInit()
 {
@@ -257,13 +206,12 @@ bool COverwriteDialog::OnInit()
     PostMsg(DM_SETDEFID, IDNO);
     HWND h = GetItem(IDNO);
     PostMsg(WM_NEXTDLGCTL, (WPARAM)h, TRUE);
-    // ::SetFocus(h);
   }
 
   if (ShowAutoRename)
-    ShowItem(IDX_AUTO_RENAME, true);
+    ShowItem_Bool(IDX_AUTO_RENAME, true);
 
-  return CModalDialog::OnInitDialog();
+  return CModalDialog::OnInit();
 }
 
 bool COverwriteDialog::OnDestroy()
@@ -272,7 +220,7 @@ bool COverwriteDialog::OnDestroy()
   SetItemIcon(IDI_OVERWRITE_OLD_FILE_2, NULL);
   SetItemIcon(IDI_OVERWRITE_NEW_FILE, NULL);
   SetItemIcon(IDI_OVERWRITE_NEW_FILE_2, NULL);
-  return false; // we return (false) to perform default dialog operation
+  return false;
 }
 
 bool COverwriteDialog::OnButtonClicked(unsigned buttonID, HWND buttonHWND)
@@ -280,16 +228,16 @@ bool COverwriteDialog::OnButtonClicked(unsigned buttonID, HWND buttonHWND)
   switch (buttonID)
   {
     case IDYES:
+    case IDB_YES_TO_ALL:
+      AutoRename = IsButtonCheckedBool(IDX_AUTO_RENAME);
+      break;
+    
     case IDNO:
-    case IDB_YES_TO_ALL:
     case IDB_NO_TO_ALL:
-    case IDYES:
-    case IDB_YES_TO_ALL:
-      AutoRename = IsButtonChecked(IDX_AUTO_RENAME);
+    case IDB_AUTO_RENAME:
+    case IDCANCEL:
       break;
   }
-  EndDialog(buttonID);
+  End((INT_PTR)buttonID);
   return true;
-}
-  return CModalDialog::OnButtonClicked(buttonID, buttonHWND);
 }
